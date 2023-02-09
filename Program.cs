29e2491ab6ab315ad2ce80582d4a4aa1f957bfb1
Ivan <mishekov6868@gmail.com>
2023-02-09 23:36:26 +0300

    3

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 3. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+
+void CreateNumbersArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("|" + array[i, j] + "|      ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+void PrintArithMean(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("|" + array[i] + "|   ");
+    }
+    Console.WriteLine();
+}
+
+
+int[,] numbers = new int[3, 4];
+CreateNumbersArray(numbers);
+PrintArray(numbers);
+
+double[] avgNumbers = new double[numbers.GetLength(1)];
+
+for (int i = 0; i < numbers.GetLength(1); i++)
+{
+    double result = 0.0;
+    for (int j = 0; j < numbers.GetLength(0); j++)
+    {
+        result += numbers[j, i];
+    }
+    avgNumbers[i] = Math.Round(result / numbers.GetLength(0), 2);
+}
+
+PrintArithMean(avgNumbers);
